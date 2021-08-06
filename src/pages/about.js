decccb074b4460eda99ff3fb8fb6579e1703e3d5
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-08-06 16:37:48 +0900

    [05] Creating a Layout File

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,27 +1,30 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        exercitationem ex delectus minima distinctio porro veniam, voluptatem
-        mollitia, perspiciatis consectetur nihil veritatis repudiandae officiis
-        reiciendis fugiat velit accusantium? Eveniet, necessitatibus!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        exercitationem ex delectus minima distinctio porro veniam, voluptatem
-        mollitia, perspiciatis consectetur nihil veritatis repudiandae officiis
-        reiciendis fugiat velit accusantium? Eveniet, necessitatibus!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        exercitationem ex delectus minima distinctio porro veniam, voluptatem
-        mollitia, perspiciatis consectetur nihil veritatis repudiandae officiis
-        reiciendis fugiat velit accusantium? Eveniet, necessitatibus!
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          exercitationem ex delectus minima distinctio porro veniam, voluptatem
+          mollitia, perspiciatis consectetur nihil veritatis repudiandae
+          officiis reiciendis fugiat velit accusantium? Eveniet, necessitatibus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          exercitationem ex delectus minima distinctio porro veniam, voluptatem
+          mollitia, perspiciatis consectetur nihil veritatis repudiandae
+          officiis reiciendis fugiat velit accusantium? Eveniet, necessitatibus!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          exercitationem ex delectus minima distinctio porro veniam, voluptatem
+          mollitia, perspiciatis consectetur nihil veritatis repudiandae
+          officiis reiciendis fugiat velit accusantium? Eveniet, necessitatibus!
+        </p>
+      </div>
+    </Layout>
   )
 }
