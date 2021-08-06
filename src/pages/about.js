c2711ca3ccc53ba26c881b0cc86168fa0672b66c
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-08-06 16:22:31 +0900

    [03] Pages & Routes

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        exercitationem ex delectus minima distinctio porro veniam, voluptatem
+        mollitia, perspiciatis consectetur nihil veritatis repudiandae officiis
+        reiciendis fugiat velit accusantium? Eveniet, necessitatibus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        exercitationem ex delectus minima distinctio porro veniam, voluptatem
+        mollitia, perspiciatis consectetur nihil veritatis repudiandae officiis
+        reiciendis fugiat velit accusantium? Eveniet, necessitatibus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        exercitationem ex delectus minima distinctio porro veniam, voluptatem
+        mollitia, perspiciatis consectetur nihil veritatis repudiandae officiis
+        reiciendis fugiat velit accusantium? Eveniet, necessitatibus!
+      </p>
+    </div>
+  )
+}
